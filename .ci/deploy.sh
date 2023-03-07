bd7e0475441d2a6abc7a7c8452880cf8e70d71c6
Joseph Mulloy <jmulloy@edx.org>
2023-03-07 14:52:04 -0500

    fix: Stop overwrite of existing chart tarballs

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -6,6 +6,17 @@ for chart in $(find ../helm-charts/ -name 'Chart.yaml'); do
   /tmp/helm dep update $(dirname ${chart})
   /tmp/helm package $(dirname ${chart})
 done
+
+# Print warning if we detect existing charts that would be overwritten
+if git status --porcelain | grep -E '^(M.|.M)'; then
+    echo "WARNING: Not overwriting the following chart(s) that already exist in the repo"
+    echo "WARNING: Please update the version(s) in Chart.yaml to upload new versions"
+    git status --porcelain | grep -E '^(M.|.M)' | awk '{print $2}'
+fi
+
+# Checkout existing files showing up as modified to prevent overwriting existing versions
+git checkout $(git status --porcelain | grep -E '^(M.|.M)' | awk '{print $2}')
+
 /tmp/helm repo index .
 git config --global user.email "admin@edx.org"
 git config --global user.name "Helm Build automation"
